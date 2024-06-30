aa600fb8781638e8cf654507f3a879eee215a473
vivajane <obuekwekosi@gmail.com>
2024-07-01 00:05:12 +0100

    fix

--- index.js
+++ index.js
@@ -12,5 +12,5 @@ updateData();
 
 setInterval(updateData, 1000);
 
-document.getElementById("slack-name").textContent = "Ojiteli Kosisochukwu";
+document.getElementById("slack-name").textContent = "KOSI OBUEKWE";
 document.getElementById("my-track").textContent = "Frontend";
